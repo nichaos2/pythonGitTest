efb019b74a1a041d8dce2a9c61a565c4c2f3e277
nichaos2 <nick.ioannou86@gmail.com>
2019-03-01 15:23:15 +0100

    AddingBaseCode

---
+++ src/main/MainRun.py
@@ -0,0 +1,11 @@
+'''
+Created on 1 mars 2019
+
+@author: nioannou
+'''
+
+from model.Person import Person
+if __name__ == '__main__':
+    person = Person()
+    print('User Abbas has been added with id ', person.set_name('Abbas'))
+    print('User associated with id 0 is ', person.get_name(0))
